a3713f6f5b860b615152eec6490783e79a0bfacf
Oleksandr Kulkov <hutofsato@gmail.com>
2025-01-06 20:03:24 +0200

    Changed styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://teahut-com-ua.github.io'
+  site: 'https://teahut-com-ua.github.io',
+  integrations: [mdx(), icon()]
 });
