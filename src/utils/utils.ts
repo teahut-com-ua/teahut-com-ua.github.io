8606106c9b192df9c36ad91bc16b715825fe3a1d
Oleksandr Kulkov <hutofsato@gmail.com>
2026-03-08 22:11:12 +0200

    Changed an Astro version and currency ratio

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -15,7 +15,7 @@ export function formatDate(date: any) {
 }
       
 export function exchangePrice(price: number) {
-    const courseRate = 44.5
+    const courseRate = 45
     const coursePrice = courseRate * price;
     return coursePrice.toFixed();
 }
