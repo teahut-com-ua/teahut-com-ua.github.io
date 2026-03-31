1fb8f25cf88b54ffe2ad0770364cfda9be7089f3
Oleksandr Kulkov <hutofsato@gmail.com>
2026-03-31 19:28:53 +0300

    Changed version

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,16 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teahut.com.ua',
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        jpeg: { mozjpeg: true },
+        webp: { effort: 6, alphaQuality: 80 },
+        avif: { effort: 4, chromaSubsampling: '4:2:0' },
+        png: { compressionLevel: 9 },
+      },
+    },
+  },
   integrations: [mdx(), icon(), sitemap()]
 });
